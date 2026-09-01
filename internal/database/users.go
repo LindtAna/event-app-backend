2782595b800ff2153196851f9491a305a8fe4442
LindtAna <anastasiialindt@gmail.com>
2026-09-02 01:30:43 +0200

    feat: Benutzerregistrierung und User-Datenbankmodell hinzugefuegt

--- internal/database/users.go
+++ internal/database/users.go
@@ -1,6 +1,10 @@
 package database
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"time"
+)
 
 type UserModel struct {
 	DB *sql.DB
@@ -11,4 +15,24 @@ type User struct {
 	Email    string `json:"email"`
 	Name     string `json:"name"`
 	Password string `json:"-"`
+}
+
+func (m *UserModel) Insert(user *User) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "INSERT INTO users(email, password, name) VALUES(?, ?, ?)"
+
+	result, err := m.DB.ExecContext(ctx, query, user.Email, user.Password, user.Name)
+	if err != nil {
+		return err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	user.Id = int(id)
+	return nil
 }
