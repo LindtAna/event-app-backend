14d60bdd5a84c948f35c0fec4c2f2ea05fd60a4c
LindtAna <anastasiialindt@gmail.com>
2026-09-02 23:53:57 +0200

    feat: Verknüpfung von Teilnehmern (Attendees) mit Events hinzugefügt

--- internal/database/users.go
+++ internal/database/users.go
@@ -12,8 +12,8 @@ type UserModel struct {
 
 type User struct {
 	Id       int    `json:"id"`
-	Email    string `json:"email"`
 	Name     string `json:"name"`
+	Email    string `json:"email"`
 	Password string `json:"-"`
 }
 
@@ -35,4 +35,24 @@ func (m *UserModel) Insert(user *User) error {
 
 	user.Id = int(id)
 	return nil
+}
+
+func (m *UserModel) Get(id int) (*User, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	query := "SELECT id, name, email, password FROM users WHERE id = ?"
+
+	var user User
+
+	err := m.DB.QueryRowContext(ctx, query, id).Scan(&user.Id, &user.Name, &user.Email, &user.Password)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &user, nil
 }
