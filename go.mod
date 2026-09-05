86f4017ed166b23e9e93bd5a65b4a8dbf9cb2ea8
LindtAna <anastasiialindt@gmail.com>
2026-09-05 22:04:57 +0200

    feat: JWT-basierte Benutzeranmeldung (Login) hinzugefügt.UserModel um GetByEmail und Hilfsmethode getUser erweitert

--- go.mod
+++ go.mod
@@ -10,6 +10,11 @@ require (
 )
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v5 v5.3.1 // indirect
+)
+
+require (
 	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic v1.15.0 // indirect
 	github.com/bytedance/sonic/loader v0.5.0 // indirect
--- internal/database/users.go
+++ internal/database/users.go
@@ -37,15 +37,13 @@ func (m *UserModel) Insert(user *User) error {
 	return nil
 }
 
-func (m *UserModel) Get(id int) (*User, error) {
+func (m *UserModel) getUser(query string, args ...interface{}) (*User, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	query := "SELECT id, name, email, password FROM users WHERE id = ?"
-
 	var user User
 
-	err := m.DB.QueryRowContext(ctx, query, id).Scan(&user.Id, &user.Name, &user.Email, &user.Password)
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.Id, &user.Name, &user.Email, &user.Password)
 
 	if err != nil {
 		if err == sql.ErrNoRows {
@@ -55,4 +53,16 @@ func (m *UserModel) Get(id int) (*User, error) {
 	}
 
 	return &user, nil
+}
+
+func (m *UserModel) Get(id int) (*User, error) {
+	query := "SELECT id, name, email, password FROM users WHERE id = ?"
+	return m.getUser(query, id)
+
+}
+
+func (m *UserModel) GetByEmail(email string) (*User, error) {
+	query := "SELECT id, name, email, password FROM users WHERE email = ?"
+	return m.getUser(query, email)
+
 }
